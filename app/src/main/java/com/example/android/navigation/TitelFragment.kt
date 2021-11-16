8a143f4c9ddfcd4110edbac3e0872185a552673c
Silvia <s.kuefe@gmx.de>
2021-11-16 18:58:34 +0100

    Creating and Adding a Fragment

---
+++ app/src/main/java/com/example/android/navigation/TitelFragment.kt
@@ -0,0 +1,33 @@
+package com.example.android.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [TitelFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class TitelFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentTitleBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+        return binding.root
+    }
+
+}
